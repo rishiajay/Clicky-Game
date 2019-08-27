16f1dabe058e21d6cb28e33d71bf186229e67dc7
rishiajay500 <rishiajay500@gmail.com>
2019-08-27 16:24:59 -0400

    Updates

---
+++ precache-manifest.d8a164f33556765fedcc59bffc15edd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57fd9d9c12be2f613450b1aa6cba9d8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "71daaf58fba0d8a61211",
+    "url": "/my-app/static/css/main.5bbe0db1.chunk.css"
+  },
+  {
+    "revision": "8426e034c4bd47d7b30e",
+    "url": "/my-app/static/js/2.29ac6cb7.chunk.js"
+  },
+  {
+    "revision": "71daaf58fba0d8a61211",
+    "url": "/my-app/static/js/main.6b865431.chunk.js"
+  },
+  {
+    "revision": "c4b01b2ad8775fe12bbf",
+    "url": "/my-app/static/js/runtime~main.98f822e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
